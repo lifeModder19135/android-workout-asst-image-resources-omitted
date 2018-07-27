6cc822cffb3b6b5b780300978fe033de71faceaa
ntolb <ntolbertu85@gmail.com>
2018-07-27 14:56:53 -0400

    -switched images for back and abs to webviews for .gif support

--- app/src/main/java/com/flacoapps/workoutassistant2/AbsDetailActivity.java
+++ app/src/main/java/com/flacoapps/workoutassistant2/AbsDetailActivity.java
@@ -2,12 +2,15 @@ package com.flacoapps.workoutassistant2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class AbsDetailActivity extends Activity {
 
     public static final String EXTRA_INFO = "absExerciseNumber" ;
+    WebView webView;
+    String webViewName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +21,19 @@ public class AbsDetailActivity extends Activity {
         int exerciseNumber = (Integer) getIntent().getExtras().get(EXTRA_INFO);
         Exercise absExercise = Exercise.absMoves[exerciseNumber];
 
+        switch (exerciseNumber){
+            case 0: webViewName = "weighted_leg_raise";
+                break;
+            case 1: webViewName = "dumbbell_side_bend";
+                break;
+            case 2: webViewName = "weighted_crunch";
+                break;
+        }
+
         //populate exercise image
-        ImageView photo = (ImageView) findViewById(R.id.abs_photo);
-        photo.setImageResource(absExercise.getImageResourceId());
-        photo.setContentDescription(absExercise.getName());
+        //ImageView photo = (ImageView) findViewById(R.id.abs_photo);
+        //photo.setImageResource(absExercise.getImageResourceId());
+        //photo.setContentDescription(absExercise.getName());
 
         //populate exercise name
         TextView name = (TextView) findViewById(R.id.name);
@@ -30,5 +42,9 @@ public class AbsDetailActivity extends Activity {
         //populate exercise description
         TextView description = (TextView) findViewById(R.id.description);
         description.setText(absExercise.getDescription());
+
+        //populate webView
+        webView = (WebView) findViewById(R.id.abs_webview);
+        webView.loadUrl("file:///android_asset/" + webViewName +".html");
     }
 }
--- app/src/main/java/com/flacoapps/workoutassistant2/BackDetailActivity.java
+++ app/src/main/java/com/flacoapps/workoutassistant2/BackDetailActivity.java
@@ -2,12 +2,15 @@ package com.flacoapps.workoutassistant2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class BackDetailActivity extends Activity {
 
     public static final String EXTRA_INFO = "backExerciseNumber" ;
+    WebView webView;
+    String webViewName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,31 @@ public class BackDetailActivity extends Activity {
         //get exercise from intent
         int backExerciseNumber = (Integer) getIntent().getExtras().get(EXTRA_INFO);
         Exercise backExercise = Exercise.backMoves[backExerciseNumber];
+
+        switch (backExerciseNumber){
+            case 0: webViewName = "back_fly";
+                break;
+            case 1: webViewName = "bend_to_opposite_foot";
+                break;
+            case 2: webViewName = "bent_over_row";
+                break;
+            case 3: webViewName = "dead_lift";
+                break;
+            case 4: webViewName = "kneeling_one_arm_row";
+                break;
+            case 5: webViewName = "one_arm_row";
+                break;
+            case 6: webViewName = "twisting_bend_to_opposite_foot";
+                break;
+            case 7: webViewName = "stiff_legged_dead_lift";
+                break;
+            case 8: webViewName = "stiff_legged_dead_lift";
+                break;
+        }
+
+        //populate webView
+        webView = (WebView) findViewById(R.id.abs_webview);
+        webView.loadUrl("file:///android_asset/" + webViewName +".html");
 
         //populate exercise image
         ImageView photo = (ImageView) findViewById(R.id.back_photo);
